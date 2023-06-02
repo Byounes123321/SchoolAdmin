bcbace15594dda82665a782134c4e97fdac95b3b
Bassil Younes <83778381+Byounes123321@users.noreply.github.com>
2023-06-02 17:13:40 -0400

    Added migrations and tried to connect to db

---
+++ SchoolAdmin/Models/IdentityModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+
+namespace SchoolAdmin.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaltConnection", throwIfV1Schema: false)        
+        {
+        }
+
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<Student> students { get; set; }
+        public DbSet<StudentXCourse> studentXCourses { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+    }
+
+
+
+}
