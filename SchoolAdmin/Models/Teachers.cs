f0e0ab922197d717ac62b184b651688741f2578b
Bassil Younes <83778381+Byounes123321@users.noreply.github.com>
2023-06-02 14:26:14 -0400

    Created Teacher model

---
+++ SchoolAdmin/Models/Teachers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolAdmin.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int TeacherId { get; set; }
+        [Required]
+        //Primary Key
+        public string Fname { get; set; }
+        //Teacher First name    
+        public string Lname { get; set; }
+        //Teacher Last name
+        public DateTime HireDate { get; set; }
+        //Date teacher was hired
+        public decimal Salary { get; set; }
+        //Teacher Salary
+
+        //A teacher has many courses 
+        public ICollection<Course> Courses;
+
+    }
+}
